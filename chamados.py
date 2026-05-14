c038a31b8ce5a4880b51aa7cbe56d73ba93b836b
caue <cauea9011@gmail.com>
2026-05-14 22:38:58 +0000

    fix: complete menu with input and options

---
+++ chamados.py
@@ -0,0 +1,28 @@
+
+
+def cadastrar_chamado():
+    pass
+
+
+def listar_chamado():
+    pass
+
+
+def buscar_chamado():
+    pass
+
+
+def atualizar_chamado():
+    pass
+
+
+def finalizar_chamado():
+    pass
+
+
+def relatorio_geral():
+    pass
+
+
+def relatorio_geral():
+    pass
