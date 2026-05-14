b79eed83fb23c7264016c63d6dd760c4970dd1a6
caue <cauea9011@gmail.com>
2026-05-14 22:20:27 +0000

    feat: add menu

---
+++ menu.py
@@ -0,0 +1,15 @@
+
+
+def Menu():
+
+    while True:
+
+        print("======== SISTEMA DE CHAMADOS =========")
+
+        print("1 - Cadastrar chamado ")
+        print("2 - Listar chamados ")
+        print("3 - Buscar chamados ")
+        print("4 - Atualizar status ")
+        print("5 - Finalizar chamado ")
+        print("6 - Status geral ")
+        print("0 - Sair ")
